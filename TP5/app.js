ab82a007f54c01034b8c1adfd881b00237835a04
alumno <52060326@ivt.edu.ar>
2026-06-04 09:08:11 -0300

    4 de junio

--- TP5/app.js
+++ TP5/app.js
@@ -1,8 +1,31 @@
-let numero1 = 25;
-let numero2 = 23;
-let resultado 
+let numero1 = 10;
+let numero2 = 10;
+let resta = 0; 
+let suma = 0;
+let multiplicacion = 0;
+let division = 0;
 
-resultado = numero1 + numero2;
+resta =  numero1 - numero2
+suma =  numero1 + numero2
+multiplicacion =  numero1 * numero2
+division =  numero1 / numero2
 
-console.log(resultado);
-alert(resultado)
+
+console.log("el resultado de la division es:" + division);
+console.log("el resultado de la multiplicacion es:" + multiplicacion)
+alert("el resultado de la suma es:" + suma)
+alert("el resultado de la resta es:" + resta)
+
+let nombrealumno = 'bautista';
+let apellidoalumno = 'peri';
+let Edadalumno = 15;
+
+console.log("el alumno es: " + nombrealumno + " " + apellidoalumno + " tiene " + Edadalumno + " años" )
+
+let precio1 = 10
+let precio2 = 20
+let precio3 = 15
+let total = precio1 + precio2 + precio3
+
+console.log("el precio a pagar es: " + total)
+
