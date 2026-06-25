9556a51db8ff103bddd6f26bd02ba20c205f18fe
alumno <alumno@Instituto.local>
2026-06-25 09:04:38 -0300

    25/6

---
+++ TP6/app.js
@@ -0,0 +1,11 @@
+let titulo = document.querySelector ('h1')
+let body = document.querySelector ('body')
+let parrafo = document.querySelector ('p')
+let boton1 = document.querySelector ('#boton1')
+let boton2 = document.querySelector ('#boton2')
+let boton3 = document.querySelector ('#boton3')
+let boton4 = document.querySelector ('#boton4')
+
+boton1.onclick = function (){
+body.backgroundColor ='white'
+}
